f4b665b7b3a579f76e1897aded693835b5df64b7
Will Ernst <wernst@users.noreply.github.com>
2023-08-18 13:23:02 -0700

    use memory storage, use updateAuthOptions to update connection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider, useSession } from "next-auth/react";
-import { TriplitClient } from "@triplit/client";
-import { PropsWithChildren, useMemo } from "react";
+import { MemoryStorage, TriplitClient } from "@triplit/client";
+import { PropsWithChildren, useEffect, useMemo } from "react";
 import { migrations, Schema } from "../../triplit/schema";
 import { TriplitClientProvider } from "@/hooks/use-triplit-client";
 
@@ -19,26 +19,30 @@ export default function App({
   );
 }
 
+const client = new TriplitClient<Schema>({
+  db: {
+    migrations: migrations,
+    storage: { cache: new MemoryStorage(), outbox: new MemoryStorage() },
+  },
+  sync: {
+    apiKey: process.env.NEXT_PUBLIC_TRIPLIT_ANON_TOKEN,
+    server: process.env.NEXT_PUBLIC_TRIPLIT_API_URL,
+    secure: true,
+  },
+});
+
 function AuthenticatedTriplitClientProvider({
   children,
 }: PropsWithChildren<{}>) {
   const session = useSession();
-  const client = useMemo(() => {
-    if (typeof window === "undefined" || !session?.data) return null;
-    return new TriplitClient<Schema>({
-      db: {
-        migrations: migrations,
-      },
-      auth: {
-        token: session.data.token,
-      },
-      sync: {
-        apiKey: process.env.NEXT_PUBLIC_TRIPLIT_ANON_TOKEN,
-        server: process.env.NEXT_PUBLIC_TRIPLIT_API_URL,
-        secure: true,
-      },
-    });
-  }, [session]);
+  useEffect(() => {
+    if (
+      typeof window === "undefined" ||
+      session?.data?.token === client.syncEngine.token
+    )
+      return;
+    client?.updateAuthOptions({ token: session?.data?.token });
+  }, [session?.data?.token]);
 
   return (
     <TriplitClientProvider value={client}>{children}</TriplitClientProvider>
